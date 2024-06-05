ad65fa573f10c60ad09f9c5c0fec1e6bb15d22a0
Keyboard Destroyer <43577127+kbrddestroyer@users.noreply.github.com>
2024-06-05 21:25:22 +0300

    Added serialization and level saver

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,16 +1,18 @@
 using GameControllers;
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 
+[Serializable]
 [CreateAssetMenu(menuName = "Items/Base Item")]
 public class Item : ScriptableObject
 {
-    public Sprite icon;
-    public string label;
-    public bool isUsable;
+    [SerializeField] public Sprite icon;
+    [SerializeField] public string label;
+    [SerializeField] public bool isUsable;
 
     public virtual void OnClick() 
     { 
